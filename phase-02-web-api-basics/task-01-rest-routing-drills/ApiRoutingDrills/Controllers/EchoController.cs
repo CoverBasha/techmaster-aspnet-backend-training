0bc375998c5dee62549b42cc85bf317eb5a2557c
CoverBasha <ahmedgerab7@gmail.com>
2026-08-25 01:11:47 +0300

    Task 1 started

---
+++ phase-02-web-api-basics/task-01-rest-routing-drills/ApiRoutingDrills/Controllers/EchoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRoutingDrills.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Echo(string name)
+        {
+            return Ok(name);
+        }
+    }
+}
---
+++ phase-02-web-api-basics/task-01-rest-routing-drills/ApiRoutingDrills/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ApiRoutingDrills.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { status = "Running", service = "TechMaster API", time = DateTime.UtcNow });
+        }
+    }
+}
